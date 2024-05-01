e818f3515da30551a1aacff437f527f92de76497
pavIov <167352418+pavIov@users.noreply.github.com>
2024-05-01 11:47:19 -0700

    Update Basic Map Lesson.py

--- Basic Map Lesson.py
+++ Basic Map Lesson.py
@@ -7,31 +7,54 @@
 #moving left decreases the x location and vice versa
 
 Map = [
-			[ 1, 1, 1, 1 ],
-			[ 1, 0, 1, 1 ],
-			[ 1, 0, 1, 1 ],
-			[ 1, 0, 0, 1 ],
-			[ 1, 0, 1, 1 ],
-			[ 1, 1, 1, 1 ]
-	]
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1],
+    [1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 1, 1, -1, -1, -1, -1, 1, 1],
+    [1, 0, 0, 0, 0, 0, -1, -1, -1, -1, 1, 1],
+    [1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, 1],
+    [1, 1, 1, 1, 1, 1, -1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
 
-
+#0=space where you can move
+#-1=minefield
+#make it so that -1 is a minefield, so, it is solid and you can
+#only pass through two mines before you die (restart)
+#you have to find the switch to deactivate the minefield to pass to the next obstacle
+#which is going to be 
 #Define a function to print our Map and current Player location
 
 def drawMap(currX, currY):
-    #print ("map size is : ", len(Map), " rows by ", len(Map[0]), " columns")
+    print ("map size is : ", len(Map), " rows by ", len(Map[0]), " columns")
     print()
-    for y in range (0, len(Map)):
-        for x in range (0,len(Map[y])):
-            if (currX == x) and (currY == y):   #print * if they are in this square
-                print("*  ", end="")
+    for y in range(0, len(Map)):
+        for x in range(0, len(Map[y])):
+            if (currX == x) and (currY == y):   # print * if they are in this square
+                print("* ", end="")
             else:
-                print (Map[y][x]," ", end ="")
+                if Map[y][x] == 0:
+                    print("  ", end="")
+                if Map[y][x] == -2:
+                    
+                elif Map[y][x] == -1:
+                    print("x", end=" ")
+                else:
+                    print(Map[y][x], "", end="")
         print()
     print()
     
 
-#Define our function that will move the player
+fc#Define our function that will move the player
 #The function will first check if the player can move or hits a wall
 #If the player can move, then the current location will be updated
 #If the player cannot move due to a wall, the location will not be updated
@@ -42,22 +65,22 @@ def movePlayer(x,y,moveDir):
 
     #Now check if the move is valid - brute force method - better ways exist
     if moveDir == "u":
-        if Map[y-1][x] == 0:
+        if Map[y-1][x] <= 0 :
             #print ("valid up move")
             return (x, y-1)
 
     if moveDir == "d":
-        if Map[y+1][x] == 0:
+        if Map[y+1][x] <= 0:
             #print ("valid down move")
             return (x, y+1)
 
     if moveDir == "l":
-        if Map[y][x-1] == 0:
+        if Map[y][x-1] <= 0:
             #print ("valid left move")
             return (x-1, y)
 
     if moveDir == "r":
-        if Map[y][x+1] == 0:
+        if Map[y][x+1] <= 0:
             #print ("valid right move")
             return (x+1, y)
         
