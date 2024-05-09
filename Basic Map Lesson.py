045ef3fb3887d8d7b5dbd7e4af3d6395e8890123
pavIov <167352418+pavIov@users.noreply.github.com>
2024-05-09 15:00:10 -0700

    HUGE Update
    *Overhauled terminal
    *Added ???
    *Improved readability of code
    *Optimized code
    *Added a win mechanic
    *Terminal now lets you disable and enable mines

--- Basic Map Lesson.py
+++ Basic Map Lesson.py
@@ -1,10 +1,18 @@
 import random
 import time
+#Philipp's Adventure Game
 ## Define a map and let the player move around it
 #Define our map: 1 = wall, 0 = path
 #0,0 is the top left of the map, therefore...
 #moving up decreases the y location and
 #moving left decreases the x location and vice versa
+WALL=1
+M=2
+SECRET=-4
+NUMBER=-9
+KEYCARD=-20
+DOOR=-3
+TERMINAL=-2
 
 Map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -14,11 +22,11 @@ Map = [
     [1, 0, 1, 1, 1, 1, 0, -4, -4, -4, -9, 1],
     [1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1],
     [1, 0, 1, 0, 0, 0, -9, 1, 1, 1, 1, 1],
-    [1, 0, 1, 0, 1, 1, -1, -1, -1, -1, 1, 1],
-    [1, 0, 0, 0, 0, 0, -1, -1, -1, -1, 1, 1],
-    [1, 0, 1, 1, 1, 1, -1, -1, -1, -1, -1, 1],
-    [1, 0, 1, 0, -9, 1, -1, 1, 1, 1, -1, -3],
-    [1, 0, 1, 0, 1, 1, -1, -1, -1, -1, -1, 1],
+    [1, 0, 1, 0, 1, 1, M, M, M, M, 1, 1],
+    [1, 0, 0, 0, 0, 0, M, M, M, M, 1, 1],
+    [1, 0, 1, 1, 1, 1, M, M, M, M, M, 1],
+    [1, 0, 1, 0, -9, 1, M, 1, 1,M, M, -3],
+    [1, 0, 1, 0, 1, 1, M, M, M, M, M, 1],
     [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 1, 1, 0, 1, 1, -20, 0, 0, -9, 1],
     [1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
@@ -27,9 +35,8 @@ Map = [
 ]
 
 #0=space where you can move
-#-1=minefield
+#2=minefield
 #-2=minefield toggle
-#?=minefield terminal
 #-9=a number
 #-3=exit
 #-4=secret passageway
@@ -46,27 +53,29 @@ def drawMap(currX, currY):
         for x in range(0, len(Map[y])):
             if (currX == x) and (currY == y):   # print * if they are in this square
                 print("* ", end="")
-            if (currX == 1) and (currY == 1) and MineStatus==True:
+            while (currX == 1) and (currY == 1) and MineStatus==True:
                 terminal()
+            if (currX == 16) and (currY == 9) and MineStatus==False:
+                win()
             else:
                 if Map[y][x] == 0:
-                    print("  ", end="")
-                elif Map[y][x] == 1:
+                    print("  ", end="") #fix issue by using if and else in the logic becaus its printing borth
+                elif Map[y][x] == WALL:
                     print("▧", end=" ")
-                elif Map[y][x] == -4:
+                elif Map[y][x] == SECRET:
                     print("▧", end=" ")
-                elif Map[y][x] == -2:
+                elif Map[y][x] == TERMINAL:
                     print("?", end=" ")
                     #terminal()
-                elif Map[y][x] == -1 and MineStatus:
+                elif Map[y][x] == 2 and MineStatus:
                     print("x", end=" ")
-                elif Map[y][x] == -1 and MineStatus==False:
+                elif Map[y][x] == M and M !=2 and  MineStatus==False:
                     print(" ", end=" ")
-                elif Map[y][x] == -3:
+                elif Map[y][x] == DOOR:
                     print("𓉞", end=" ")
-                elif Map[y][x] == -20:
+                elif Map[y][x] == KEYCARD:
                     print(" ", end=" ")
-                elif Map[y][x] == -9:
+                elif Map[y][x] == NUMBER:
                     print(" ", end=" ")
                 
                 else:
@@ -83,6 +92,7 @@ def welcomeMsg():
 #If the player cannot move due to a wall, the location will not be updated
     
 def movePlayer(x,y,moveDir):
+    global M
     #assume invalid move is attempted
     badMove = True
 
@@ -113,24 +123,37 @@ def movePlayer(x,y,moveDir):
         return (x,y)   #return the same location they are in since no move
 #made a terminal that is a question mark on the map; when player steps up to terminal he gets prompted to disable mines
 def terminal():
-    MineStatus=True
+    InteractionComplete=False #making a flag so player doesn't get stuck at the terminal forever
+    MineStatus=True #change logic because mines will activate every time you enter terminal
+    print (num1)
     code=int(input("TERMINAL3847: greetings, AGENT. Please enter 5-digit access code: "))
-    if code==(num1, num2, num3, num4, num5): #make random num for code
+    if code==(num1): #make random num for code
         MinePrompt=input("disable mines? (y/n)")
         if MinePrompt=="y":
             MineStatus=False
+            InteractionComplete=True
+        if MinePrompt=="n":
+            Boomprompt=input("detonate Mines? (y/n)")
+            if Boomprompt=="y":
+                print("boom") #I will make it so that the prompt from the events() function activates
+                InteractionComplete=True
+                if Boomprompt=="n":
+                    print ("Diagnostics complete, have a nice day, AGENT")
+                    InteractionComplete=True
+    if MineStatus==False:
+        M=-1
     else:
         print ("Invalid Code!")
+        InteractionComplete=True
+    if  InteractionComplete==True:
+        ("🃑")
+        return (currX+1, currY)
     return MineStatus
 #making a mechanic for the terminal code, so that it is completely random for higher replayability
-def DungeonCode(num1, num2, num3, num4, num5):
-    num1=random.randint(0,9)
-    num2=random.randint(0,9)
-    num3=random.randint(0,9)
-    num4=random.randint(0,9)
-    num5=random.randint(0,9)
+def DungeonCode(num1):
+    num1=random.randint(10001, 99999)
     #print ("dungeon code is", num1, num2, num3, num4, num5)
-    return num1, num2, num3, num4, num5
+    return num1
 
 def events():
     Chance=random.randint(0,30)
@@ -147,32 +170,31 @@ def countdown(time_sec, Toggle):
     while time_sec:
         mins, secs = divmod(time_sec, 60)
         timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        #print(timeformat, end='\r')
+        print(timeformat, end='\r')
         time.sleep(1)
         time_sec -= 1
 
     print("demise imminent")
     Toggle=True
     return Toggle
+def win():
+    print("You have won,", name, ". You run up the stairs of the dungeon and hear the room collapsing behind you. \n if you waited a second longer, you would have probably died")
 #Set our starting location
 currX = 1
 currY = 4
 num1=0
-num2=0
-num3=0
-num4=0
-num5=0
+DungeonCode(num1)
 AutoDetonation=0
 #adding global variables to be used by the functions
 MineStatus=True
 #draw the map the first time before asking for a move
 welcomeMsg()
 drawMap(currX, currY)
-DungeonCode(0, 0, 0, 0, 0)
+
 #Forever just let the player move around the map on the path
 
 while True:
-    moveDir = input("Enter direction (W, A, S, D): ")
+    moveDir = input("Enter direction (w, a, s, d): ")
     currX, currY = movePlayer(currX, currY, moveDir)
     drawMap(currX, currY)
 
